c1d0335049932b1cb9d9954af8df1a9af2afdab3
KalashnikovTV <max.malaschkov@mail.ru>
2020-08-19 00:23:20 +0300

    a little practice

--- learn.javascript.ru/somePractice/test.js
+++ learn.javascript.ru/somePractice/test.js
@@ -42,3 +42,49 @@ let arr = [1, 2, 3, 4, 5];
 
 arr.pop();
 console.log(arr);
+
+
+// вывод чётных чисел
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+
+let browser = 'Firefox';
+
+if (browser == 'Edge') {
+    alert('You\'ve got the Edge!');
+} else if (browser == 'Chrome' || browser == 'Firefox' || browser == 'Safari' || browser == 'Opera') {
+    alert('Okay we support these browsers too');
+} else {
+    alert( 'We hope that this page looks ok!' );
+}
+
+
+// Переписанный код на switch
+const number = +prompt('Введите число между 0 и 3', '');
+
+switch (number) {
+    case 0:
+        alert('Вы ввели число 0');
+        break;
+    case 1:
+        alert('Вы ввели число 1');
+        break;
+    case 2:
+    case 3:
+        alert('Вы ввели число 2, а может и 3');
+        break;
+}
+
+function min(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+min(2, 5);
+min(1, 1);
