c2cd78c61e0d146d8875465e512dd46de2f69cc6
KalashnikovTV <max.malaschkov@mail.ru>
2020-10-13 22:00:24 +0300

    Iterators and generators #2

--- learn.javascript.ru/IteratorsGenerators/script.js
+++ learn.javascript.ru/IteratorsGenerators/script.js
@@ -164,3 +164,152 @@ for (let value of iterator11) {
 
 
 // Создание итерируемых объектов на ES6
+let obj12 = {
+    1: "one",
+    2: "two",
+    3: "three"
+};
+
+function* createIterator(obj) {
+    for (let key in obj) {
+        yield obj[key];
+    }
+}
+let iterator12 = createIterator(obj12);
+for (let elem of iterator12) {
+    console.log(elem);
+}
+
+
+// Встроенные итераторы коллекций values, keys и entries
+let arr13 = [1, 2, 3, 4];
+let iterator13 = arr13.entries();
+
+for (let [key, value] of iterator13) {
+    console.log(key, value);
+}
+
+
+
+let arr14 = [1, 2, 3, 4, 5];
+let iterator14 = arr14.keys();
+let sum = 0;
+
+for (let key of iterator14) {
+    sum += key;
+}
+console.log(sum);
+
+
+
+let map15 = new Map();
+map15.set('a', 1);
+map15.set('b', 2);
+
+let iterator15 = map15.keys();
+
+for (let key of iterator15) {
+    console.log(key);
+}
+
+
+
+let map16 = new Map();
+map16.set('a', 1);
+map16.set('b', 2);
+
+let iterator16 = map16.values();
+
+for (let value of iterator16) {
+    console.log(value);
+}
+
+
+// Встроенные итераторы строк
+let str17 = 'abcde';
+let ul17 = document.querySelector('.task17');
+
+for (let elem of str17) {
+    let li = document.createElement('li');
+    li.innerHTML = elem;
+    ul17.appendChild(li);
+}
+
+
+
+let num18 = 123456;
+let summ = 0;
+
+for (let value of String(num18)) {
+    summ += +value;
+}
+console.log(summ);
+
+
+// Оператор spread и итераторы
+console.log([...document.querySelectorAll('.task19')]);
+
+
+
+let obj20 = {
+    1: "a",
+    2: "b",
+    3: "c",
+    [Symbol.iterator]: function* () {
+        for (let key in this) {
+            yield this[key];
+        }
+    }
+};
+console.log(...obj20);
+
+
+
+let obj21 = {
+    1: "a",
+    2: "b",
+    3: "c",
+    [Symbol.iterator]: function* () {
+        for (let key in this) {
+            yield key;
+        }
+    }
+};
+console.log(...obj21);
+
+
+// Оператор spread и строки
+function arrSymb(str) {
+    return console.log([...str]);
+}
+arrSymb('abcde');
+
+
+
+function arrSymbRev(str) {
+    return console.log([...str].reverse());
+}
+arrSymbRev('abcde');
+
+
+
+let num24 = 123456;
+let sum24 = 0;
+
+for (let value of String(num24)) {
+    sum24 += +value;
+}
+console.log(sum24);
+
+
+// Применение
+let parags = document.querySelectorAll('.task25');
+let btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function() {
+    let iterator = parags.entries();
+    for (let [num, elem] of iterator) {
+        console.log(num, elem);
+        elem.innerHTML += num;
+    }
+});
